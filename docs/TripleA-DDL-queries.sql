cf749003734c238ec8644c158f5bcae7b2649832
Aru <180107217@stu.sdu.edu.kz>
2021-03-06 17:48:24 +0600

    ddl queries without FK

--- docs/TripleA-DDL-queries.sql
+++ docs/TripleA-DDL-queries.sql
@@ -7,7 +7,55 @@ CREATE TABLE employee (
     yearsAtCompany INT(2)
 );
 
-CREATE TABLE manager  (  
+CREATE TABLE manager (  
     managerID INT(11) NOT NULL PRIMARY KEY,
     name VARCHAR(50)
+);
+
+CREATE TABLE company (  
+    companyID INT(11) NOT NULL PRIMARY KEY,
+    employeeID INT(11) NOT NULL,
+    name VARCHAR(50),
+    employeeNumber INT(5)
+);
+
+CREATE TABLE project (  
+    projectID INT(11) NOT NULL PRIMARY KEY,
+    employeeID INT(11) NOT NULL,
+    managerID INT(11) NOT NULL,
+    projectName VARCHAR(50)
+);
+
+CREATE TABLE job (  
+    jobID INT(11) NOT NULL PRIMARY KEY,
+    jobLevel INT(5),
+    jobRole VARCHAR(11)
+);
+
+CREATE TABLE task (  
+    taskID INT(11) NOT NULL PRIMARY KEY,
+    jobID INT(11) NOT NULL,
+    employeeRecordID  INT(11) NOT NULL,
+    taskDescription VARCHAR(50)
+);
+
+CREATE TABLE employee_additional_info (  
+    employeeAdditionalInfoID INT(11) NOT NULL PRIMARY KEY,
+    employeeID INT(11) NOT NULL,
+    education INT(2),
+    maritalStatus VARCHAR(5),
+    monthlyIncome INT(10),
+    environmentSatisfaction INT(5),
+    trainingTimesLastYear INT(5)
+);
+
+CREATE TABLE employee_record (  
+    employeeRecordID INT(11) NOT NULL PRIMARY KEY,
+    employeeID INT(11) NOT NULL,
+    performanceRating INT(5),
+    spentTime TIME,
+    monthlyIncome INT(10),
+    standardHours INT(5),
+    inTime DATETIME,
+    outTime DATETIME
 );
