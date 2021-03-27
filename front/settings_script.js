75be7ab982974a4d26cade826fcff55e4f4b37e8
Ayymzhan <180107110@stu.sdu.edu.kz>
2021-03-28 01:47:49 +0300

    setting page

--- front/settings_script.js
+++ front/settings_script.js
@@ -17,16 +17,49 @@ function login(){
 
 
 
-var newPassword = document.getElementById("new_password");
-var confirmPassword = document.getElementById("confirm_password");
-
-function validatePassword() {
-    if (newPassword.value != confirmPassword.value) {
-        document.getElementById("confirm_password").setCustomValidity("Passwords do not match!");
-    } else {
-        //empty string means no validation error
-        document.getElementById("confirm_password").setCustomValidity('');
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks!";
     }
-}
-newPassword.addEventListener("change", validatePassword);
-confirmPassword.addEventListener("change", validatePassword);
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+  
