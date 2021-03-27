2446b4d654751fdc92571db3faa47bfd01803982
Ayymzhan <180107110@stu.sdu.edu.kz>
2021-03-28 01:07:29 +0300

    some changes

--- front/settings_script.js
+++ front/settings_script.js
@@ -12,3 +12,21 @@ function login(){
     y.style.left = "450px";
     z.style.left = "0";
 }
+
+
+
+
+
+var newPassword = document.getElementById("new_password");
+var confirmPassword = document.getElementById("confirm_password");
+
+function validatePassword() {
+    if (newPassword.value != confirmPassword.value) {
+        document.getElementById("confirm_password").setCustomValidity("Passwords do not match!");
+    } else {
+        //empty string means no validation error
+        document.getElementById("confirm_password").setCustomValidity('');
+    }
+}
+newPassword.addEventListener("change", validatePassword);
+confirmPassword.addEventListener("change", validatePassword);
