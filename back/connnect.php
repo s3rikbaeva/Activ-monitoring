9ff3a2b49e0be78cb3929be79b194779a6bd75a9
Aru <180107217@stu.sdu.edu.kz>
2021-03-28 23:42:18 +0600

    Merge full app

--- back/connnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $connect = mysqli_connect('localhost', 'root', 'root', 'test');
-    
-    if(!$connect){
-        die('Error connect to DataBase');
-    }
-?>
--- back/profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (!$_SESSION['user']) {
-    header('Location: index.php');
-}
-?>
