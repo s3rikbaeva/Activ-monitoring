2cc9cd28f834bdb1a5500d8d8e6f4f0bf20cc87a
Aru <180107217@stu.sdu.edu.kz>
2021-03-28 23:30:47 +0600

    adding app

--- back/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $connect = mysqli_connect('localhost', 'root');
-    if ($connect->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-?>
