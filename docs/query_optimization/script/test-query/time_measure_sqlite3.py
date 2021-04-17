10f71c6274055f57a814caf4eb95f1015ca7ac96
Aru <180107217@stu.sdu.edu.kz>
2021-04-17 15:11:59 +0600

    5th query

--- docs/query_optimization/script/test-query/time_measure_sqlite3.py
+++ docs/query_optimization/script/test-query/time_measure_sqlite3.py
@@ -32,9 +32,11 @@ def testQuery(_conn):
 
     try:
         sql = """ 
-            SELECT er.employeeID, (er.standardHours-er.spentTime) as "remaining_time" 
-            FROM employee_record er, project p, employee e 
-            WHERE e.employeeID = er.employeeID AND e.employeeID = p.employeeID AND projectID = 100;
+            SELECT e.employeeID
+            FROM employee e, project p, employee_record er
+            WHERE e.employeeID = p.employeeID
+            AND e.employeeID = er.employeeID
+            AND e.employeeID = (SELECT employeeID FROM employee WHERE inTime = CURRENT_DATE);
         """
         print(sql)
 
