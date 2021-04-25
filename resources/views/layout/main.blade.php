efb064cc2cfa084cd39055e9bd0b3d5fa0dd3fa7
Aru <180107217@stu.sdu.edu.kz>
2021-04-25 18:16:47 +0600

    changes

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Login</title>
-        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="stylesheet" href="css/main.css">
         <link href="{{asset('https://fonts.googleapis.com/css?family=Poppins:600&display=swap')}}" rel="stylesheet">
         <script src="{{asset('https://kit.fontawesome.com/a81368914c.js')}}"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
